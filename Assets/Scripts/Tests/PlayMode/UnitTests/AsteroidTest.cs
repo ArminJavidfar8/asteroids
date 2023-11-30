dcd85e3a5cf2d60c2a0028957585816f7fa32554
arminjavidfar@live.com <2580761519>
2023-11-30 15:25:03 +0330

    improve asteroids movement test

--- Assets/Scripts/Tests/PlayMode/UnitTests/AsteroidTest.cs
+++ Assets/Scripts/Tests/PlayMode/UnitTests/AsteroidTest.cs
@@ -3,6 +3,7 @@ using Management.Core;
 using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
 using Services.Abstraction;
+using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.TestTools;
@@ -15,14 +16,19 @@ namespace Tests.PlayMode.UnitTest
         public IEnumerator TestAsteroidMovement()
         {
             IAsteroidsService asteroidsService = ServiceHolder.ServiceProvider.GetService<IAsteroidsService>();
-            IAsteroid asteroid = asteroidsService.AddAsteroid(AsteroidType.Small);
 
-            Vector3 beforeMovementPosition = asteroid.Position;
-            asteroid.MoveProperly();
+            Array allAsteroidTypes = Enum.GetValues(typeof(AsteroidType));
+            for (int i = 1; i < allAsteroidTypes.Length; i++)
+            {
+                IAsteroid asteroid = asteroidsService.AddAsteroid((AsteroidType)allAsteroidTypes.GetValue(i));
 
-            yield return new WaitForSeconds(0.2f);
+                Vector3 beforeMovementPosition = asteroid.Position;
+                asteroid.MoveProperly();
 
-            Assert.AreNotEqual(beforeMovementPosition, asteroid.Position);
+                yield return new WaitForSeconds(0.2f);
+
+                Assert.AreNotEqual(beforeMovementPosition, asteroid.Position);
+            }
         }
 
         [UnityTearDown]
